180a40649748eff03a9f2abba196cc8db5c916c3
brofar <behfar@otafest.com>
2022-07-28 16:10:12 -0600

    Totals tracker working

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ Work in progress.
 
 This is a simple tracker for Extra Life donations for Slurpathon.
 
-- [x] Web tracker
+- [x] Web tracker - index.html
     - [ ] Remove sample data
-- [ ] Stream overlay
+- [x] Stream overlay - counter.html  (or counter.html?dark for use on dark BGs)
+    - [ ] Remove sample data
 - [ ] Incentive tracker
 - [ ] Bid war tracker
---
+++ js/counter.js
@@ -0,0 +1,59 @@
+// For counter.html
+// Depends on extralife-api.js
+
+import { CountUp } from "./countup.min.js";
+const options = {
+      decimalPlaces: 2,
+      prefix: '$',
+    }
+let counter = new CountUp('counter', 0, options);
+
+// Global variables
+//let counter = document.getElementById('counter');
+let raised = 0;
+
+
+async function CheckTotals() {
+    let team = await GetTeam(settings.entityID);
+    let newRaised = team.sumDonations;
+    if (newRaised > raised) {
+        counter.update(newRaised);
+        raised = newRaised;
+    }
+}
+
+async function test(num) {
+    await sleep(3000);
+    counter.update(num);
+}
+
+// Our main function
+async function main () {
+    // Check for dark mode
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const dark = (urlParams.get('dark') != null);
+
+    if(dark) {
+        document.getElementById('counter').classList.add('dark');
+    }
+
+    counter.start();
+
+    // Update counter
+    //CheckTotals();
+
+    // Check every 30 seconds.
+    //setInterval(CheckTotals, 30000);
+
+    //! TEST DATA
+    await test(51);
+    await test(61);
+    await test(70.5);
+    await test(70.98);
+    await test(1000000);
+
+}
+
+main();
+
