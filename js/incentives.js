6780dd95bcd01fc2bace0a68dbc93f1197bfbcda
brofar <behfar@otafest.com>
2022-09-11 17:18:25 -0600

    Working incentives bar

--- js/incentives.js
+++ js/incentives.js
@@ -1,24 +1,16 @@
+// Get spreadsheet info
 const sheetId = "1TfXc0pIIUl0W5HcKnWHPRx4TlbHueX2DWg8yvtjAEeY";
 const apiKey = "";
 const range = "DB!A:G";
 const sheetUrl = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?dateTimeRenderOption=FORMATTED_STRING&majorDimension=ROWS&valueRenderOption=UNFORMATTED_VALUE&key=${apiKey}`;
 
-const incentivesToShow = 3;
+// Get slide timer value
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const timer = urlParams.get('timer');
+const slideTimer = (timer != null) ? timer : 5000;
 
-(async () => {
-    let json = await GetData();
-    let values = json.values;
-
-    // remove headers
-    values.splice(0, 1);
-
-    let filtered = FilterCurrentIncentives(values);
-    let parsed = ParseSheet(filtered);
-
-    SetDiv(parsed);
-    console.log(parsed);
-})();
-
+/* =================== GOOGLE =================== */
 // Grab data from Google Sheets API
 async function GetData() {
     const res = await fetch(sheetUrl, {
@@ -26,73 +18,170 @@ async function GetData() {
     });
 
     let json = await res.json();
+    let values = json.values;
+    let headerRow = values.splice(0, 1)[0];
+    let data = [];
 
-    return json;
+    for (const row of values) {
+        let obj = {};
+        for (let i = 0; i < headerRow.length; i++) {
+            obj[headerRow[i].toLowerCase()] = row[i];
+        }
+        data.push(obj);
+    }
+
+    return data;
+}
+
+// Remove past incentives from the object
+function FilterCurrentIncentives(values) {
+    // values.sort((a, b) => a[2] - b[2]);
+    return values.filter(arr => arr.active === true);
 }
 
 // Convert string values to proper types
-function ParseSheet(input) {
+function ProcessValues(input) {
 
-    let incentives = {};
+    let incentives = [];
 
     for (let i = 0; i < input.length; i++) {
-        let row = input[i];
-
-        let obj = {
-            game: row[0],
-            type: row[1],
-            visible: row[2] || false,
-            goal: row[3] == "" ? 0 : row[3],
-            total: row[4] == "" ? 0 : row[4],
-            choice: row[5],
-            description: row[6]
-        }
+        let obj = input[i];
 
         obj.combinedname = obj.game + " - " + obj.description;
 
-        if (obj.type.toLowerCase() == "bid war") {
+        let type = obj.type.toLowerCase();
+
+        if (type == "bid war") {
+            const warExists = el => el.combinedname === obj.combinedname;
+            let index = incentives.findIndex(warExists);
+
             // Create object if not exists
-            if (!incentives.hasOwnProperty(obj.combinedname))
-                incentives[obj.combinedname] = {
-                    type: "war",
-                    choices: {}
-                };
+            if (index === -1) {
+                let warObj = obj;
+                warObj.choices = {};
+                index = incentives.length;
+                incentives.push(warObj);
+            }
 
-            incentives[obj.combinedname].choices[obj.choice] = obj.total;
+            incentives[index].choices[obj.choice] = obj.total;
 
-        } else if (obj.type.toLowerCase() == "goal") {
-            incentives[obj.combinedname] = {
-                type: "goal",
-                goal: obj.goal,
-                total: obj.total
-            };
+        } else if (type == "goal") {
+            incentives.push(obj);
         }
     }
 
     return incentives;
 }
 
-// Remove past incentives from the object
-function FilterCurrentIncentives(values) {
-    // values.sort((a, b) => a[2] - b[2]);
-    return values.filter(arr => arr[2] === true);
+function CalculateBidWarTotals(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].hasOwnProperty("choices")) {
+            let sumValues = Object.values(arr[i].choices).reduce((a, b) => a + b);
+            arr[i].sum = sumValues;
+        }
+    }
+
+    return arr;
 }
 
-function SetDiv(values) {
-    let div = document.getElementById("incentive-bar");
-    if (div.firstChild) {
-        // It has at least one child
-    } else {
-        for (const property in values) {
-            //console.log(`${property}: ${values[property]}`);
-            let child = BuildDiv(property, values[property]);
-            div.appendChild(child);
+/* =================== SLIDESHOW =================== */
+
+// using above code we can get Transition end event name
+const transitionEndEventName = GetTransitionEndEventName();
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function GetTransitionEndEventName() {
+    var transitions = {
+        "transition": "transitionend",
+        "OTransition": "oTransitionEnd",
+        "MozTransition": "transitionend",
+        "WebkitTransition": "webkitTransitionEnd"
+    }
+    let bodyStyle = document.body.style;
+    for (let transition in transitions) {
+        if (bodyStyle[transition] != undefined) {
+            return transitions[transition];
         }
     }
 }
 
-// https://thomaswilburn.github.io/viz-book/css-flex.html
-function BuildDiv(property, values) {
+async function BuildSlide(title, arr) {
+    // Create the overall bar
+    let progress = document.createElement('div');
+    progress.classList.add('bro-progress');
+
+    let counters = []
+
+    // Create counters
+    for (const el of arr) {
+        let left = document.createElement('div');
+        left.classList.add('bro-bar', 'bro-counter', 'bro-left');
+        left.innerHTML = el.name;
+        left.setAttribute("data-width", el.percent * 100);
+        left.style.backgroundColor = el.color;
+        counters.push(left);
+    }
+
+    // Add counters to DOM
+    for (const counter of counters) {
+        progress.appendChild(counter);
+    }
+
+    // Create the rest of the remaining bar
+    let right = document.createElement('div');
+    right.classList.add('bro-bar', 'bro-remainder');
+    progress.appendChild(right);
+
+    // Create the title
+    let descriptor = document.createElement('div');
+    descriptor.innerHTML = title;
+    descriptor.classList.add('descriptor');
+
+    // Create the container
+    let slide = document.createElement('div');
+    slide.classList.add('slide');
+    slide.appendChild(descriptor);
+    slide.appendChild(progress);
+
+    return slide;
+}
+
+async function PlaySlide(title, arr) {
+    let slide = await BuildSlide(title, arr);
+
+    // Add the bar to the page
+    let main = document.getElementsByTagName('main')[0];
+    main.appendChild(slide);
+
+    // Animate the values
+    await sleep(100);
+    slide.style.opacity = 1;
+
+    // Set width
+    var els = document.getElementsByClassName("bro-counter");
+    Array.from(els).forEach((el) => {
+        el.style.width = el.getAttribute('data-width') + '%';
+    });
+
+    await sleep(slideTimer);
+    slide.style.opacity = 0;
+
+    await new Promise((resolve) => {
+        slide.addEventListener(transitionEndEventName, resolve, { once: true });
+    });
+
+    slide.remove();
+
+    return Promise.resolve();
+}
+
+/* =================== PUT IT ALL TOGETHER =================== */
+
+async function Main(predata = null) {
+    let data;
     const colors = [
         "#EAB8D5",
         "#CC3F61",
@@ -103,49 +192,62 @@ function BuildDiv(property, values) {
         "#46220F",
         "#D59D88",
         "#F3EAF9"
-    ]
-    let child = document.createElement("div");
-    if (values.type == "war") {
-        child.className = "bar-container";
+    ];
 
-        let sumValues = Object.values(values.choices).reduce((a, b) => a + b);
+    if (predata == null) {
+        console.log('Live data');
+        let raw = await GetData();
 
-        let i = 0;
-        for (const option in values.choices) {
-            let choice = document.createElement("div");
-            choice.className = "bar";
-            choice.style = `flex-basis: ${(values.choices[option] / sumValues) * 100}%; background-color: ${colors[i]}`;
-            choice.innerHTML = option + ": " + values.choices[option];
-            child.appendChild(choice);
-            i++;
-        }
-
-        console.log("");
-    } else if (values.type == "goal") {
-        child.className = "bar-container";
-        
-        let choice = document.createElement("div");
-        let remainder = document.createElement("div");
-
-        choice.className = "bar";
-        let percentage = (values.total / values.goal) * 100
-        choice.style = `flex-basis: ${percentage}%; background-color: salmon; justify-content: right;`;
-        let text = property + ": " + values.total + " / " + values.goal;
-
-        if(percentage < 40) {
-            remainder.innerHTML = text;
-        } else {
-            choice.innerHTML = text;
-        }
-
-        // fill the rest of the bar with a transparent value
-        remainder.className = "bar";
-        remainder.style = `flex-basis: ${100 - percentage}%; justify-content: left;`;
-        
-        child.appendChild(choice);
-        child.appendChild(remainder);
-
-        console.log(percentage);
+        let filtered = FilterCurrentIncentives(raw);
+        let parsed = ProcessValues(filtered);
+        data = CalculateBidWarTotals(parsed);
+    } else {
+        console.log('Cached data');
+        data = predata;
     }
-    return child;
+
+    for (const incentive of data) {
+        let title = incentive.combinedname;
+        let type = incentive.type.toLowerCase();
+        let arr = [];
+        let i = 0;
+        if (type == "bid war") {
+            let choices = incentive.choices;
+            for (const property in choices) {
+                let obj = {
+                    name: `${property} $${choices[property]}`,
+                    percent: choices[property] / incentive.sum,
+                    color: colors[i]
+                }
+                arr.push(obj);
+                i++;
+            }
+        } else if (type == "goal") {
+            arr = [{
+                name: `$${incentive.total} / $${incentive.goal}`,
+                percent: incentive.total / incentive.goal,
+                color: colors[0]
+            }];
+        }
+
+        await PlaySlide(title, arr);
+    }
+
+    return Promise.resolve(data);
 }
+
+async function LetsGo() {
+    let i = 3;
+    let cache;
+    while (true) {
+        if(i >= 3) {
+            cache = await Main();
+            i = 0;
+        } else {
+            await Main(cache);
+        }
+        i++;
+    }
+}
+
+LetsGo();
